e7530fea5d720945836da40244d45ae6d481c64a
unknown <mohammadalmobayed4@gmail.com>
2022-12-11 23:35:09 +0200

    j

---
+++ sunday12_11/task2.php
@@ -0,0 +1,7 @@
+<?php
+
+// echo "Tomorrow I \'ll learn PHP global variables."."<br>" . "This is a bad command : del c";
+echo nl2br("Tomorrow I 'll learn PHP global variables.'
+'This is a bad command: del c:."
+)
+?>
